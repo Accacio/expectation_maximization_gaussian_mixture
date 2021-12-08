ed59b4ed45a490abbfcabe5086ebc94ed4b83843
Accacio <raccacio@poli.ufrj.br>
2021-12-08 17:55:16 +0100

    plot with respective colors

--- lab.m
+++ lab.m
@@ -1,15 +1,30 @@
-% clear;close all;
+clear;close all;
 
 % Generate and plot data
 [prob,mu,sigma,z,x]=generate_data(500,2);
-scatter(x(1,:),x(2,:),10)
+figure(1)
+hold on
+colors={'b','g','r'};
+for i=1:3
+    z_i=find(z==i);
+    x_i=x(:,z_i);
+    scatter(x_i(1,:),x_i(2,:),10,colors{i})
+end
 
 % Plot gaussians for comparison
-hold on
-plot_gaussian(mu(:,1),sigma(:,:,1),'b')
-plot_gaussian(mu(:,2),sigma(:,:,2),'g')
-plot_gaussian(mu(:,3),sigma(:,:,3),'r')
+for i=1:3
+    plot_gaussian(mu(:,i),sigma(:,:,i),colors{i})
+end
 title("Ground Truth")
 ylim([1 7.5])
 xlim([0.1 7.5])
+hold off
+
+figure(2)
+hold on
+for i=1:3
+    z_i=find(z==i);
+    x_i=x(:,z_i);
+    scatter(x_i(1,:),x_i(2,:),10,colors{i})
+end
 hold off
